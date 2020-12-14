ba46a32991dac082c4470d0e3d6d88561193fc6f
Danny GIllespie <danny.gillespie@hotmail.com>
2020-12-14 13:56:31 +0000

    First Commit
    Everything so far...

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+    public float increment;
+    public float maxY;
+    public float minY;
+
+    private Vector2 targetPos;
+
+    public int health;
+
+    public GameObject moveEffect;
+    public GameObject deathEffect;
+    public Animator camAnim;
+    public Text healthDisplay;
+
+    public GameObject spawner;
+    public GameObject restartDisplay;
+
+    public GameObject moveSound;
+    public GameObject deathSound;
+
+
+    private void Update()
+    {
+        if (health <= 0) {
+            Instantiate(deathSound, transform.position, Quaternion.identity);
+            spawner.SetActive(false);
+            restartDisplay.SetActive(true);
+            Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+        healthDisplay.text = health.ToString();
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxY) {
+            Instantiate(moveSound, transform.position, Quaternion.identity);
+            camAnim.SetTrigger("shake");
+            Instantiate(moveEffect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y + increment);
+        } else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minY) {
+            Instantiate(moveSound, transform.position, Quaternion.identity);
+            camAnim.SetTrigger("shake");
+            Instantiate(moveEffect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y - increment);
+        }
+    }
+}
